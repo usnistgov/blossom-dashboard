95f5b5ae485e36cc404f5f03567e8c75c637417d
Nikita Wootten <nikita.wootten@nist.gov>
2022-01-18 17:24:37 -0500

    Changed arg parsing behavior

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,13 @@
 import express from 'express';
 import * as fs from 'fs';
 import { Command } from 'commander';
+import YAML from 'yaml';
 import Blossom from './blossom';
 import buildTransactionRoute from './routes/transaction';
 import buildIdentityRoute from './routes/indentities';
 import { Config } from './config';
 
-interface Options {
-    /**
-     * Path to config yaml file
-     */
-    config: string;
-}
-
-function parseArgs() {
-    const args = new Command()
-        .version('0.0.1')
-        .description('BLOSSOM Relay Server')
-        .argument('<config>', 'config yaml file')
-        .parse(process.argv);
-
-    return args as unknown as Options;
-}
-
-async function init() {
-    const options = parseArgs();
-    const config = JSON.parse(fs.readFileSync(options.config).toString());
-    
-    const blossom = await Blossom.build(config as Config)
-
+function mapRoutes(blossom: Blossom, port: string) {
     const app = express();
 
     // Enable JSON serialization
@@ -41,9 +20,21 @@ async function init() {
         res.status(200).send('ok');
     });
 
-    app.listen(process.env.SERVER_PORT, () => {
-        console.log(`server started at http://0.0.0.0:${process.env.SERVER_PORT}`);
+    app.listen(port, () => {
+        console.log(`server started at http://0.0.0.0:${port}`);
     });
 }
 
-init();
+const program = new Command()
+    .version('0.0.1')
+    .description('BLOSSOM Relay Server')
+    .argument('<configPath>', 'config yaml file')
+    .option('-p --port', 'Port to run the server on', "8080")
+    .action(async (configPath, options) => {
+        const config = (YAML.parse(fs.readFileSync(configPath).toString())) as Config;
+        const blossom = await Blossom.build(config as Config);
+        
+        mapRoutes(blossom, options.port);
+    });
+
+program.parse(process.argv);
