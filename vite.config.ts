0e92e33c71aa04641094d3e21958604763a342f7
Nikita Wootten <nikita.wootten@nist.gov>
2022-11-02 13:07:39 -0400

    Remove `DEFAULT_PROXY_URL` from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // for deployments that are deployed to a sub-folder like /dev, specify below or use the `BASE_URL` env var
 const DEFAULT_BASE_URL = "/";
-// to proxy transaction api requests on a different domain, specify below or use the `PROXY_URL` env var
-const DEFAULT_PROXY_URL = "https://719hgl5ejk.execute-api.us-east-1.amazonaws.com/dev";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/transaction": {
-        target: process.env.PROXY_URL ?? DEFAULT_PROXY_URL,
+        target: process.env.PROXY_URL,
         // Fixes SSL error on some requests
         changeOrigin: true
       }
