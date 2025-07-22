bcb3cc96f5f82dbf492abc1c703b1cfadfc299f6
Cousin <dmitry.cousin@nist.gov>
2025-07-22 17:32:41 -0400

    fix: modified dev-init script

--- dev-init.sh
+++ dev-init.sh
@@ -1,1 +1,1 @@
-cp /Users/dac4/github/!!!Blossom-Backup-Profiles/.env.development /Users/dac4/github/blossom-dashboard/.env.development
+cp ../Blossom-Backup-Profiles/.env.development /Users/dac4/github/blossom-dashboard/.env.development
